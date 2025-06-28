14329458a230a2885fbd91df849a7e8da51be7b3
EngBasmaa <basmaelmasry263@gmail.com>
2025-06-28 19:50:10 +0300

    add pwa files

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'flowbite';
-import 'flowbite/dist/flowbite.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "flowbite";
+import "flowbite/dist/flowbite.css";
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
