e83cc4833cfee04bf2d46db30a0dfa28c9e630b5
EngBasmaa <basmaelmasry263@gmail.com>
2025-06-30 00:11:38 +0300

    handling installing button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,10 +16,7 @@ export function Navbar() {
     window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
     return () => {
-      window.removeEventListener(
-        "beforeinstallprompt",
-        handleBeforeInstallPrompt
-      );
+      window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     };
   }, []);
 
@@ -38,13 +35,9 @@ export function Navbar() {
     }
   };
 
-  return (
-    <nav className="bg-white px-5 border-gray-200 dark:bg-gray-900">
+  return <nav className="bg-white px-5 border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto pt-4 px-5">
-        <NavLink
-          to="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
+        <NavLink to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="/logo/myLogo.png" className="h-8" alt="Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
             IDesign
@@ -53,78 +46,41 @@ export function Navbar() {
 
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-3 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <i>
-              {/* install button */}
-              {showInstallBtn &&
-                <button
-                  className="bg-yellow-500 px-2 py-1 rounded hover:bg-yellow-600 cursor-pointer "
-                  onClick={handleInstallClick}
-                >
-                  install now
-                </button>}
-            </i>
             <li>
-              <NavLink
-                to="/"
-                className={({ isActive }) =>
-                  isActive
-                    ? "block py-2 px-3 text-secondary font-bold md:p-0"
-                    : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"}
-              >
+              <NavLink to="/" className={({ isActive }) => (isActive ? "block py-2 px-3 text-secondary font-bold md:p-0" : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent")}>
                 Home
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/about"
-                className={({ isActive }) =>
-                  isActive
-                    ? "block py-2 px-3 text-secondary font-bold md:p-0"
-                    : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"}
-              >
+              <NavLink to="/about" className={({ isActive }) => (isActive ? "block py-2 px-3 text-secondary font-bold md:p-0" : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent")}>
                 About
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/services"
-                className={({ isActive }) =>
-                  isActive
-                    ? "block py-2 px-3 text-secondary font-bold md:p-0"
-                    : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"}
-              >
+              <NavLink to="/services" className={({ isActive }) => (isActive ? "block py-2 px-3 text-secondary font-bold md:p-0" : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent")}>
                 Services
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/projects"
-                className={({ isActive }) =>
-                  isActive
-                    ? "block py-2 px-3 text-secondary font-bold md:p-0"
-                    : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"}
-              >
+              <NavLink to="/projects" className={({ isActive }) => (isActive ? "block py-2 px-3 text-secondary font-bold md:p-0" : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent")}>
                 Projects
               </NavLink>
             </li>
 
             <li>
-              <NavLink
-                to="/contact"
-                className={({ isActive }) =>
-                  isActive
-                    ? "block py-2 px-3 text-secondary font-bold md:p-0"
-                    : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"}
-              >
+              <NavLink to="/contact" className={({ isActive }) => (isActive ? "block py-2 px-3 text-secondary font-bold md:p-0" : "block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-secondary md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent")}>
                 Contact
               </NavLink>
             </li>
           </ul>
         </div>
+        {/* install button */}
+        {showInstallBtn && <button className="bg-yellow-500 px-2 py-1 rounded hover:bg-yellow-600 cursor-pointer " onClick={handleInstallClick}>
+            install now
+          </button>}
       </div>
-    </nav>
-  );
+    </nav>;
 }
